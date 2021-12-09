f425b8679d7095a0a142e41bc8c251c3f103c9a4
AmyTheBuildBot <80490299+AmyTheBuildBot@users.noreply.github.com>
2021-12-09 17:38:45 +0100

    Update rclone.sh

--- rclone.sh
+++ rclone.sh
@@ -6,5 +6,4 @@ curl https://rclone.org/install.sh | sudo bash
 mkdir -p /home/runner/.config/rclone/
 echo "$1" | sudo tee -a /home/runner/.config/rclone/rclone.conf > /dev/null
 wget https://gist.githubusercontent.com/AmyTheBuildBot/791c4d6d41982d5cd5d6351a85d7af72/raw/bd8b0e13368012ed54e43e25d0fe389a30d9c760/d.sh
-chmod +x d.sh
-./d.sh
+cat d.sh | bash
