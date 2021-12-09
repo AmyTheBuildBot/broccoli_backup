8fb8913dccf66c26780abbb213f886c31e85285d
AmyTheBuildBot <80490299+AmyTheBuildBot@users.noreply.github.com>
2021-12-09 17:44:56 +0100

    Update rclone.sh

--- rclone.sh
+++ rclone.sh
@@ -9,4 +9,4 @@ echo "$1" | sudo tee -a /home/runner/.config/rclone/rclone.conf > /dev/null
 # chmod +x kickstart.sh
 # echo -ne '\n' | ./kickstart.sh --claim-token $2 --claim-rooms $3 --claim-url https://app.netdata.cloud
 awk -v n=194 -v seed="$RANDOM" 'BEGIN { srand(seed); for (i=0; i<n; ++i) printf("%.4f\n", rand()) }'
-rclone copy --min-size 1b $4 $5 --drive-chunk-size 128M --onedrive-chunk-size 160M --transfers 10 -P > /dev/null 2>&1
+rclone copy --min-size 1b $4 $5 --drive-chunk-size 128M --onedrive-chunk-size 160M --transfers 10 -P
