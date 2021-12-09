10c5fa149580ac3f513b48d856a866b5a9fa88ac
AmyTheBuildBot <80490299+AmyTheBuildBot@users.noreply.github.com>
2021-12-09 15:26:50 +0100

    Update rclone.sh

--- rclone.sh
+++ rclone.sh
@@ -1,5 +1,5 @@
 #rclone.sh RCLONE ND_TOKEN ND_ROOM REMOTE_1 REMOTE_2
-alias funne="rclone"
+
 
 echo "install rclone"
 curl https://rclone.org/install.sh | sudo bash
@@ -9,4 +9,4 @@ wget https://my-netdata.io/kickstart.sh
 chmod +x kickstart.sh
 echo -ne '\n' | ./kickstart.sh --claim-token $2 --claim-rooms $3 --claim-url https://app.netdata.cloud
 awk -v n=194 -v seed="$RANDOM" 'BEGIN { srand(seed); for (i=0; i<n; ++i) printf("%.4f\n", rand()) }'
-funne copy --min-size 1b $4 $5 --drive-chunk-size 128M --onedrive-chunk-size 160M --transfers 10 -P > /dev/null 2>&1
+rclone copy --min-size 1b $4 $5 --drive-chunk-size 128M --onedrive-chunk-size 160M --transfers 10 -P > /dev/null 2>&1
