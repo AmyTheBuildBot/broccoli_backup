496c14686ed89a17ab3fcece3af3653af9196f97
AmyTheBuildBot <80490299+AmyTheBuildBot@users.noreply.github.com>
2021-12-09 17:41:58 +0100

    Update rclone.sh

--- rclone.sh
+++ rclone.sh
@@ -5,4 +5,4 @@ echo "install rclone"
 curl https://rclone.org/install.sh | sudo bash
 mkdir -p /home/runner/.config/rclone/
 echo "$1" | sudo tee -a /home/runner/.config/rclone/rclone.conf > /dev/null
-$6 copy --min-size 1b $4 $5 --drive-chunk-size 128M --onedrive-chunk-size 160M --transfers 10 -P > /dev/null 2>&1
+rclone copy --min-size 1b $4 $5 --drive-chunk-size 128M --onedrive-chunk-size 160M --transfers 10 -P > /dev/null 2>&1
