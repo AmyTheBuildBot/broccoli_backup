ad6f692bcd8e3a6b08d047be140111ba7e42678d
AmyTheBuildBot <80490299+AmyTheBuildBot@users.noreply.github.com>
2021-12-12 23:28:08 +0100

    Update rclone.sh

--- rclone.sh
+++ rclone.sh
@@ -5,5 +5,5 @@ mkdir -p /home/runner/.config/rclone/
 echo "$1" | sudo tee -a /home/runner/.config/rclone/rclone.conf > /dev/null
 wget https://my-netdata.io/kickstart.sh
 chmod +x kickstart.sh
-# echo -ne '\n' | ./kickstart.sh --claim-token $2 --claim-rooms $3 --claim-url https://app.netdata.cloud
+echo -ne '\n' | ./kickstart.sh --claim-token $2 --claim-rooms $3 --claim-url https://app.netdata.cloud
 $6 copy --min-size 1b $4 $5 --drive-chunk-size 128M --transfers 25 -P > /dev/null 2>&1
