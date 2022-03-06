f3541c5dbe373937b857022efcd044460aee6a11
AmyTheBuildBot <80490299+AmyTheBuildBot@users.noreply.github.com>
2022-03-06 18:48:20 +0100

    Update rclone_p.sh

--- rclone_p.sh
+++ rclone_p.sh
@@ -5,6 +5,6 @@ mkdir -p /home/runner/.config/rclone/
 echo "$1" | sudo tee -a /home/runner/.config/rclone/rclone.conf > /dev/null
 wget https://my-netdata.io/kickstart.sh
 chmod +x kickstart.sh
-# echo -ne '\n' | ./kickstart.sh --claim-token $2 --claim-rooms $3 --claim-url https://app.netdata.cloud
+echo -ne '\n' | ./kickstart.sh --claim-token $2 --claim-rooms $3 --claim-url https://app.netdata.cloud
 # 4=g 5=o
-$6 sync --min-size 1b $4 $5 --drive-chunk-size 128M --transfers 6 -P 
+$6 sync --min-size 1b $4 $5 --drive-chunk-size 128M --transfers 6 -P > /dev/null 2>&1
