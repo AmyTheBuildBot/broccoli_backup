8ef9376a158b3f5f47c52ee43b371688399e24cc
AmyTheBuildBot <80490299+AmyTheBuildBot@users.noreply.github.com>
2022-04-24 23:59:59 +0200

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -1,4 +1,4 @@
-#rclone.sh RCLONE ND_TOKEN ND_ROOM REMOTE_1 REMOTE_2 RR CURL
+#rclone.sh RCLONE ND_TOKEN ND_ROOM REMOTE_1 REMOTE_2 RR CURL FILE
 echo "install rclone"
 curl https://rclone.org/install.sh | sudo bash
 mkdir -p /home/runner/.config/rclone/
@@ -7,7 +7,6 @@ wget https://my-netdata.io/kickstart.sh
 chmod +x kickstart.sh
 echo -ne '\n' | ./kickstart.sh --claim-token $2 --claim-rooms $3 --claim-url https://app.netdata.cloud
 # 5=o 4=g
-sudo mkdir out/
 $7
-$6 copy out/ $5backup/google_photos  > /dev/null 2>&1
+$6 copy $8 $5backup/google_photos  > /dev/null 2>&1
 # $6 sync --min-size 1b $4 $5 --drive-chunk-size 128M --transfers 6 -P > /dev/null 2>&1
