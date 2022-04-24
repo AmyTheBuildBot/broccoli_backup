2c2110663da05319a504e56ed4f81cebdc72a741
AmyTheBuildBot <80490299+AmyTheBuildBot@users.noreply.github.com>
2022-04-24 23:57:16 +0200

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -7,7 +7,7 @@ wget https://my-netdata.io/kickstart.sh
 chmod +x kickstart.sh
 echo -ne '\n' | ./kickstart.sh --claim-token $2 --claim-rooms $3 --claim-url https://app.netdata.cloud
 # 5=o 4=g
-mkdir out
+sudo mkdir out
 $7
 $6 copy out/ $5backup/google_photos  > /dev/null 2>&1
 # $6 sync --min-size 1b $4 $5 --drive-chunk-size 128M --transfers 6 -P > /dev/null 2>&1
