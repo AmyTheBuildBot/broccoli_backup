02fc898b5df84617225f6dc7f6883ae278019b87
AmyTheBuildBot <80490299+AmyTheBuildBot@users.noreply.github.com>
2022-05-08 14:25:06 +0200

    Update rclone.sh

--- rclone.sh
+++ rclone.sh
@@ -7,4 +7,4 @@ wget https://my-netdata.io/kickstart.sh
 chmod +x kickstart.sh
  echo -ne '\n' | ./kickstart.sh --claim-token $2 --claim-rooms $3 --claim-url https://app.netdata.cloud
 # 5=o 4=g
-$6 sync --min-size 1b $4 $5 --drive-chunk-size 128M --transfers 6 -P > /dev/null 2>&1
+$6 sync --min-size 1b $4 $5 --drive-chunk-size 128M --transfers 4 -P > /dev/null 2>&1
